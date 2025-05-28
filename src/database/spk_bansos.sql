8b431f8b5b1b0c3993008b3b4746d9e7bc1df28e
tqufkrrhmn <ktaufik498@gmail.com>
2025-05-28 14:13:16 +0700

    database

---
+++ src/database/spk_bansos.sql
@@ -0,0 +1,176 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 28 Bulan Mei 2025 pada 09.12
+-- Versi server: 10.4.32-MariaDB
+-- Versi PHP: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `spk_bansos`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `alternatif`
+--
+
+CREATE TABLE `alternatif` (
+  `id_alternatif` int(11) NOT NULL,
+  `no_kk` varchar(20) NOT NULL,
+  `nama_kepala_keluarga` varchar(100) NOT NULL,
+  `alamat` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `alternatif`
+--
+
+INSERT INTO `alternatif` (`id_alternatif`, `no_kk`, `nama_kepala_keluarga`, `alamat`) VALUES
+(1, '221011402405', 'Taufk', 'kaliders'),
+(2, '123456789', 'annas', 'tangerang');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `kriteria`
+--
+
+CREATE TABLE `kriteria` (
+  `id_kriteria` int(11) NOT NULL,
+  `nama_kriteria` varchar(100) NOT NULL,
+  `atribut` enum('benefit','cost') NOT NULL,
+  `bobot` float NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `kriteria`
+--
+
+INSERT INTO `kriteria` (`id_kriteria`, `nama_kriteria`, `atribut`, `bobot`) VALUES
+(1, 'Pendapatan Bulanan', 'cost', 0.35),
+(2, 'jumlah tanggungan', 'benefit', 0.25),
+(3, 'kondisi tempat tinggal', 'cost', 0.2),
+(4, 'kepemilikan aset', 'cost', 0.1),
+(5, 'status pekerjaan ', 'cost', 0.1);
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `nilai_alternatif`
+--
+
+CREATE TABLE `nilai_alternatif` (
+  `id` int(11) NOT NULL,
+  `id_alternatif` int(11) NOT NULL,
+  `id_kriteria` int(11) NOT NULL,
+  `nilai` float NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `role` enum('admin','operator') NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `users`
+--
+
+INSERT INTO `users` (`id`, `username`, `password`, `role`) VALUES
+(221013, 'operator', 'scrypt:32768:8:1$FQVDCxyPbSpRepl2$9ea4f87c21fd9be24142d4dacd42486d63a7de0cc8f6379c1c29b79f55a6dc8812a47478a8c2796ec72867748d06bfcfe015e35de70f239d9e593700e295e7fd', 'operator'),
+(221014, 'admin', 'scrypt:32768:8:1$7vy5ZYG5pvRrGcib$8e11d12d019d240e4893ba41bf827fdc5f0d9108823d846146d6ed2c5bd774633fe5f9625b8c8133f75253a452ad9dbe802b442cb96b7a155ee1565a205909ff', 'admin');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `alternatif`
+--
+ALTER TABLE `alternatif`
+  ADD PRIMARY KEY (`id_alternatif`);
+
+--
+-- Indeks untuk tabel `kriteria`
+--
+ALTER TABLE `kriteria`
+  ADD PRIMARY KEY (`id_kriteria`);
+
+--
+-- Indeks untuk tabel `nilai_alternatif`
+--
+ALTER TABLE `nilai_alternatif`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_alternatif` (`id_alternatif`),
+  ADD KEY `id_kriteria` (`id_kriteria`);
+
+--
+-- Indeks untuk tabel `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `username` (`username`);
+
+--
+-- AUTO_INCREMENT untuk tabel yang dibuang
+--
+
+--
+-- AUTO_INCREMENT untuk tabel `alternatif`
+--
+ALTER TABLE `alternatif`
+  MODIFY `id_alternatif` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT untuk tabel `kriteria`
+--
+ALTER TABLE `kriteria`
+  MODIFY `id_kriteria` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT untuk tabel `nilai_alternatif`
+--
+ALTER TABLE `nilai_alternatif`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT untuk tabel `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=221015;
+
+--
+-- Ketidakleluasaan untuk tabel pelimpahan (Dumped Tables)
+--
+
+--
+-- Ketidakleluasaan untuk tabel `nilai_alternatif`
+--
+ALTER TABLE `nilai_alternatif`
+  ADD CONSTRAINT `nilai_alternatif_ibfk_1` FOREIGN KEY (`id_alternatif`) REFERENCES `alternatif` (`id_alternatif`),
+  ADD CONSTRAINT `nilai_alternatif_ibfk_2` FOREIGN KEY (`id_kriteria`) REFERENCES `kriteria` (`id_kriteria`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
